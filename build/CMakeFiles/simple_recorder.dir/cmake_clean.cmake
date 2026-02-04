3bd9ad4acd356ab557228b4d9c26161044bbe571
lorenaquintino <m.wendh@gmail.com>
2026-02-04 12:16:25 -0300

    Primeira versão

---
+++ build/CMakeFiles/simple_recorder.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_recorder.dir/main.cpp.o"
+  "CMakeFiles/simple_recorder.dir/main.cpp.o.d"
+  "simple_recorder"
+  "simple_recorder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_recorder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
